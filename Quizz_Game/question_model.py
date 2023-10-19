53cf90fbe6fa991328b45d44ce952a036df9d874
dannybaylon28 <danielbaylon2885@icloud.com>
2023-10-18 21:49:03 -0600

    First Commit

---
+++ Quizz_Game/question_model.py
@@ -0,0 +1,52 @@
+class Question():
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
