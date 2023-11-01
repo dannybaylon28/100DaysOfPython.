0077ac665a7af07077024be64a654cc7b89047a7
dannybaylon28 <danielbaylon2885@icloud.com>
2023-10-31 21:48:21 -0600

    First Commit

--- Us_States_Games/main.py
+++ Us_States_Games/main.py
@@ -6,41 +6,28 @@ screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
 data = pandas.read_csv("50_states.csv")
-states = data["state"].to_list()
-correct_guesses = []
+all_states = data.state.to_list()
+guessed_states = []
 
-game_on = True
-while game_on:
-    if len(correct_guesses) < 50:
-        answer_state = screen.textinput(title=f"{len(correct_guesses)}/50 States Correct", prompt="Guess the state's name").title()
-        if answer_state == "Exit":
-            missing_states = []
-            for state in states:
-                if state not in correct_guesses:
-                    missing_states.append(state)
-            new_data = pandas.DataFrame(missing_states)
-            new_data.to_csv("states_to_learn.csv")
-            break
-        if answer_state in states:
-            # Jalar las coordenadas de cada estado.
-            x_cor = int(data[data["state"] == answer_state]['x'].values[0])
-            y_cor = int(data[data["state"] == answer_state]['y'].values[0])
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
-            # Escribir el estado en el mapa
-            turtle.penup()
-            turtle.tracer(0.5)
-            turtle.goto(x_cor, y_cor)
-            turtle.write(answer_state)
-
-            # Guardar Respuestas Adivinadas
-            correct_guesses.append(answer_state)
-    else:
-        game_on = False
-        turtle.exitonclick()
-        print("Thanks for playing")
-
-
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
 # states_to_learn.csv
 
