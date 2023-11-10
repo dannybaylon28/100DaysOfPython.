034f29bc04b2eeeb2a0a1cd2427b82549389922c
dannybaylon28 <danielbaylon2885@icloud.com>
2023-11-10 01:02:59 -0600

    First Commit

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -1,13 +1,14 @@
 import pandas
 
-# TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 names = pandas.read_csv("nato_phonetic_alphabet.csv")
 names_dict = {row.letter: row.code for (index, row) in names.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_name = input("Type your name. ").upper()
-coded_name = [names_dict[f"{char}"] for char in user_name]
-print(coded_name)
-
-
+is_on = True
+while is_on:
+    try:
+        user_name = input("Type your name. ").upper()
+        coded_name = [names_dict[f"{char}"] for char in user_name]
+        print(coded_name)
+        is_on = False
+    except KeyError:
+        print("Sorry, only letters please.")
--- password-manager/main.py
+++ password-manager/main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
-
+import json
 FONT_NAME = "Courier"
 
 
@@ -28,17 +28,49 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Empty Fields", message="Please don't leave any of the fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                              f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{website} | {email} |{password} \n")
-                website_entry.delete(0, 'end')
-                password_entry.delete(0, 'end')
+        try:
+            with open("data.json", "r") as file:
+                # Reading old data
+                data = json.load(file)
+
+        except FileNotFoundError:
+            with open("data.json", "w") as file:
+                # Saving update data
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
+
+            with open("data.json", "w") as file:
+                # Saving update data
+                json.dump(data, file, indent=4)
+
+        finally:
+            website_entry.delete(0, 'end')
+            password_entry.delete(0, 'end')
+
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+            if website in data:
+                messagebox.showinfo(title="Email and Password", message=f"Email: {data[website]['email']}"
+                                                                        f"\nPassword: {data[website]['password']} ")
+            else:
+                messagebox.showinfo(title="No website found", message="No details for the website exists.")
+    except FileNotFoundError:
+        messagebox.showinfo(title="File Not Found", message="No Data File Found.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -53,9 +85,12 @@ canvas.grid(column=1, row=0)
 
 website_label = Label(text="Website: ", font=("Arial", 8))
 website_label.grid(column=0, row=1)
-website_entry = Entry(window, width=48, font=("Arial", 9), bg="white")
+website_entry = Entry(window, width=30, font=("Arial", 9), bg="white")
 website_entry.grid(column=1, row=1, columnspan=2, padx=2, pady=2, sticky="w")
 website_entry.focus()
+
+search_button = Button(text="Search", command=find_password, width=13)
+search_button.grid(column=2, row=1)
 
 email_label = Label(text="Email/Username: ", font=("Arial", 8))
 email_label.grid(column=0, row=2)
