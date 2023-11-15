5066230d9062d91ddf7cc98ce5dc67534b1c734d
Daniel Baylon <danielbaylon2885@icloud.com>
2023-11-15 11:07:45 -0600

    Commit

---
+++ quizzler-app/main.py
@@ -0,0 +1,56 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+from ui import QuizInterface
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+#     response.raise_for_status()
+#     data = response.json()
