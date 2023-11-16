a42745a82e00143a0690c8187b868d4a71863bd4
Daniel Baylon <danielbaylon2885@icloud.com>
2023-11-16 14:49:48 -0600

    First Commit

--- Weather_api/main.py
+++ Weather_api/main.py
@@ -6,7 +6,7 @@ import os
 from twilio.rest import Client
 from twilio.http.http_client import TwilioHttpClient
 
-OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+OWM_Endpoint = "API KEY"
 api_key = os.environ.get("OWM_API_KEY")
 account_sid = "YOUR ACCOUNT SID"
 auth_token = os.environ.get("AUTH_TOKEN")
